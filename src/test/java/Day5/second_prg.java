91a86885c7d3e8d0ff89bc378c8d62fdb60d10f2
unknown <saravananpushparaj@gmail.com>
2016-01-17 12:57:33 +0530

    V1.0

---
+++ src/test/java/Day5/second_prg.java
@@ -0,0 +1,11 @@
+package Day5;
+
+public class second_prg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Raghu and Vinoth");
+
+	}
+
+}
